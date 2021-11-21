de1ab489fde6778204269fc30bde97c763e66ffe
treasure-hunter-py <94384053+treasure-hunter-py@users.noreply.github.com>
2021-11-22 00:33:26 +0200

    Update home-work-1.py

--- home-work-1.py
+++ home-work-1.py
@@ -1,5 +1,8 @@
 '''1. Написати скрипт, який конкатенує всі елементи в списку і виведе їх на екран. Список можна "захардкодити".
    Елементами списку повинні бути як рядки, так і числа.'''
    
-test_list_input = ['aaaa','bbbb','cccc','dddd','eeee','ffff','gggg','jjjj']
-print(''.join(test_list_input))
+test_list_input = ['aaaa','bbbb','cccc','dddd','eeee','ffff','gggg','jjjj', 123, 222222222, 555555]
+up_task = ''
+for i in test_list_input:
+    up_task += str(i)
+print(up_task)
